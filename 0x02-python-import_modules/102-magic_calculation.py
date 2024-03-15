83c6efdd61b26072e1b9e34bcbd18fe6c96c9140
ouchafra <ismail.soso9@gmail.com>
2024-03-14 18:15:32 -0700

    0x02. Python - import & modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
+from magic_calculation_102 import add, sub
 
+
+def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
         return (c)
-
     else:
-        return(sub(a, b))
+        return sub(a, b)
