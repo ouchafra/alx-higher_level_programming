1a3921bf05d23e8cb67a413e4bb2d5610470b3a0
ouchafra <ismail.soso9@gmail.com>
2024-03-18 11:09:54 -0700

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    first_char = sentence[0] if length > 0 else "None"
-    tup = length, first_char
-    return(tup
+    if not sentence:
+        sentence = None
+    if sentence:
+        sen_len = len(sentence)
+    else:
+        sen_len = 0
+    return (sen_len, sentence if not sentence else sentence[:1])
