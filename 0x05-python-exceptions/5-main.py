a9e7fdbecea7cae33f19e973bd66d1d13d7f8574
ouchafra <ismail.soso9@gmail.com>
2024-04-02 18:53:16 -0700

    0x05. Python - Exceptions

---
+++ 0x05-python-exceptions/5-main.py
@@ -0,0 +1,8 @@
+guillaume@ubuntu:~/0x05$ cat 5-main.py
+#!/usr/bin/python3
+raise_exception = __import__('5-raise_exception').raise_exception
+
+try:
+    raise_exception()
+except TypeError as te:
+    print("Exception raised")
