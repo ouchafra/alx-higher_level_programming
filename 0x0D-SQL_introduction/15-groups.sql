df61ce01a3898afeb473d1c59febc1659e643d07
ouchafra <ismail.soso9@gmail.com>
2024-05-23 13:01:37 +0100

    0x0D-SQL_introduction

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- this lists the number of records with the same score in the table second_table in my MySQL server.
+-- this Records are ordered by descending count.
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
