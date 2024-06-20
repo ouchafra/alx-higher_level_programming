51c70623a324c3a636919feffd59d09a91a62a34
ouchafra <ismail.soso9@gmail.com>
2024-06-20 13:30:24 -0700

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
-    console.log('No argument');
-  } else {
-    console.log(process.argv[2]);
-  }
+
+const args = process.argv;
+
+if (args[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(args[2]);
+}
