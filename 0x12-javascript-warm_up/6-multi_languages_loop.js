70af0c44a37253fe4060265ea1f1371fd3d0882d
ouchafra <ismail.soso9@gmail.com>
2024-06-20 13:37:39 -0700

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-    console.log(line);
-  }
+
+const content = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const element of content) {
+  console.log(element);
+}
