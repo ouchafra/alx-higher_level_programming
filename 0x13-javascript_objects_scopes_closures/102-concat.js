4e2d0fdac68fd05470cf69722323fafbc171a012
ouchafra <ismail.soso9@gmail.com>
2024-06-20 13:14:48 -0700

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,25 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-// Get the file paths from command line arguments
-const [, , fileA, fileB, fileC] = process.argv;
-
-console.log(`File A: ${fileA}`);
-console.log(`File B: ${fileB}`);
-console.log(`File C: ${fileC}`);
-
-try {
-  // Read fileA and fileB synchronously
-  const dataA = fs.readFileSync(fileA, 'utf8');
-  const dataB = fs.readFileSync(fileB, 'utf8');
-
-  // Concatenate dataA and dataB
-  const concatenatedData = dataA + dataB;
-
-  // Write concatenated data to fileC
-  fs.writeFileSync(fileC, concatenatedData, 'utf8');
-
-  console.log(`Files ${fileA} and ${fileB} successfully concatenated to ${fileC}`);
-} catch (err) {
-  console.error('Error:', err.message);
-}
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
