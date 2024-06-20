39457cd9ddedd0b8e6b007a18110dac2d21dd40c
ouchafra <ismail.soso9@gmail.com>
2024-06-20 13:35:37 -0700

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
-    console.log('Not a number');
-  } else {
-    console.log('My number:', parseInt(process.argv[2]));
-  }
+
+const num = parseInt(process.argv[2]);
+
+if (!isNaN(num) && Number.isInteger(num)) {
+  console.log('My number:', num);
+} else {
+  console.log('Not a number');
+}
