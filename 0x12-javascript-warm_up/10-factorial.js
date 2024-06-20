740e385370cf5eebc397954f94c234c73330b486
ouchafra <ismail.soso9@gmail.com>
2024-06-20 13:47:02 -0700

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
 function factorial (n) {
-    if (isNaN(n)) {
-      return 1;
-    }
-    if (n === 0 || n === 1) {
-      return 1;
-    }
-    return n * factorial(n - 1);
+  if (isNaN(n)) {
+    return 1;
   }
-  
-  const fact = factorial(process.argv[2]);
-  console.log(fact);
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const fact = factorial(process.argv[2]);
+console.log(fact);
