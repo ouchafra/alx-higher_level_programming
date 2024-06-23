ab1ecef9c0dc5fd3b02d03bca81617e67054b0b1
ouchafra <ismail.soso9@gmail.com>
2024-06-23 15:06:52 -0700

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
-"""List all states"""
-from sys import argv
+"""
+    A script that lists all State objects from the database hbtn_0e_6_usa
+    Username, password and dbname wil be passed as arguments to the script.
+"""
+
+
+import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2],
-                argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+
+    # create a session
     session = Session()
-    for state in session.query(State).order_by(State.id).all():
+
+    # extract all states
+    states = session.query(State).order_by(State.id).all()
+
+    # print all states
+    for state in states:
         print("{}: {}".format(state.id, state.name))
+
     session.close()
